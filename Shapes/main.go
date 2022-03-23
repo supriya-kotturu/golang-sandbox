092b91518d08670af5d95875b1234dcf876cdea7
supriya-kotturu <supriyakotturu@gmail.com>
2022-03-23 22:37:45 +0530

    add | Interfaces and Structs

---
+++ Shapes/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func main() {
+
+	s := square{sideLength: 24}
+	t := triangle{height: 14, base: 7}
+	printArea(s)
+	printArea(t)
+
+}
