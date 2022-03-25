10c2dc2cb78b021057af5ef5b49d3d3c13df4dcb
supriya-kotturu <supriyakotturu@gmail.com>
2022-03-26 00:36:21 +0530

    add | channels and routines

--- Channels/main.go
+++ Channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -10,17 +11,26 @@ func main() {
 		[]string{
 			"https://google.com", "https://stackoverflow.com", "https://gnu.org",
 		}
+	c := make(chan string)
 
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(27 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println(link, " might be up down")
+		fmt.Println(link, " is down =(")
+		c <- link
 	}
-
 	fmt.Println(link, " is up!")
+	c <- link
 }
